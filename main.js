3a74a2a94bdef95feb612d20900fe31426d8356c
whitehat0917 <whitehat0917@gmail.com>
2021-07-05 07:18:35 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// jQuery(document).ready(function(){
+// 	$("#shirt_loadmore").on('click', function(){
+
+// 	});
+// });
+function checkWin() {
+	if (jQuery("#check-number1").val() != ''){
+		jQuery("#balance-container").removeClass("hidden");
+	}
+}
